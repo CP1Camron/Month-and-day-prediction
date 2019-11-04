ed6682d788d19403cdd843ded76ed0f710a368c5
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-11-04 09:16:10 -0500

    Added the Nursery Rhyme

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,20 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("There's a nursery rhyme that goes like...");
+
+        System.out.println("Monday's child is fair of face, ");
+        System.out.println("Tuesday's child is full of grace,");
+        System.out.println("Wednesday's child is full of woe,");
+        System.out.println("Thursday's child has far to go");
+        System.out.println("Friday's child is loving and giving,");
+        System.out.println("Saturday's child works hard for a living,");
+        System.out.println("But the child born on the sabbath day,");
+        System.out.println("Is fair and wise and good in every way.");
+
+
+    }
+}
