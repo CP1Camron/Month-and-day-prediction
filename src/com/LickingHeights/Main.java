77256679e1db2e8f8c75e1725eb9cdbfc5742274
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-11-21 09:12:40 -0500

    added switch's and cases and return

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -22,7 +22,7 @@ public class Main {
         System.out.println("But the child born on the sabbath day,");
         System.out.println("Is fair and wise and good in every way.");
 
-        System.out.println("What is the day you were born?");
+        System.out.println("What is the day you were born?(The number)");
         day = keyboard.nextLine();
 
         System.out.println("What was the month you were born");
@@ -31,8 +31,22 @@ public class Main {
         System.out.println("And finally what year were you born");
         year = keyboard.nextLine();
 
-        System.out.print("So you were born "+day);
-        System.out.print(" "+month);
-        System.out.print(" "+year);
+        System.out.print("So you were born "+month);
+        System.out.print("/"+day);
+        System.out.print("/"+year);
+    }
+    public static int month(int month, int day, int year){
+
+        switch(month){
+            case 1:
+                return 2;
+            case 3:
+                return 4;
+            case 5:
+                return 6;
+            case 7:
+                return 8:
+
+        }
     }
 }
